bab0dd1376bc33a37755b7c0be51783e26dce931
KevinPgn <kevin.pigeon2810@gmail.com>
2024-05-20 13:33:41 +0200

    correction bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,9 @@ export default function RootLayout({
       <body className={"flex min-h-screen px-5 " + inter.className} data-theme='night'>
         <SessionProvider>
           <Sidebar />
-          <section className="flex p-3 w-full overflow-y-auto">
+          <section 
+          style={{scrollBehavior: 'smooth'}}
+          className="flex p-3 h-screen w-full overflow-y-auto">
             <div className="w-full">
             <div className="flex items-center justify-around">
               <SearchBar />
